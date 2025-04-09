e2c3a07a47f5f829685ce59776157fc27a15d234
FABOOBA08 <fabianandresamayalancheros@gmail.com>
2025-04-08 22:28:15 -0500

    feat: cambios en layout, tailwind y fuentes

---
+++ tailwind.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+      './src/**/*.{html,js,jsx,ts,tsx,astro}',
+      './components/**/*.{html,js,jsx,ts,tsx,astro}',
+    ],
+    darkMode: 'class',
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
